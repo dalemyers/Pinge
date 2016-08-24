8467305191dbe70f4afaa31e416fa1c681cdb5f3
Dale Myers <dalemy@microsoft.com>
2016-08-24 09:08:48 +0100

    Initial Commit

---
+++ Pinge/Pinge.h
@@ -0,0 +1,19 @@
+//
+//  Pinge.h
+//  Pinge
+//
+//  Created by Dale Myers on 24/08/2016.
+//  Copyright © 2016 Dale Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pinge.
+FOUNDATION_EXPORT double PingeVersionNumber;
+
+//! Project version string for Pinge.
+FOUNDATION_EXPORT const unsigned char PingeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pinge/PublicHeader.h>
+
+
