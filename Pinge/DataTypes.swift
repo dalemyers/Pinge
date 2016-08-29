24c96343f78ae1d37eb033c83041857c5e9867e8
Vel0x <dale@myers.io>
2016-08-29 15:58:23 +0100

    Unfilter

--- Pinge/DataTypes.swift
+++ Pinge/DataTypes.swift
@@ -14,6 +14,21 @@ public enum ColorType: Int {
   static func isValidValue(colorType: Int) -> Bool {
     return colorType >= 0 && colorType <= 6 && colorType != 1 && colorType != 5
   }
+
+  func samples() -> Int {
+    switch self {
+    case .Greyscale:
+      return 1
+    case .TrueColor:
+      return 3
+    case .IndexedColor:
+      return 1
+    case .GreyscaleWithAlpha:
+      return 2
+    case .TruecolorWithAlpha:
+      return 4
+    }
+  }
 }
 
 public enum InterlaceMethod: Int {
@@ -22,4 +37,12 @@ public enum InterlaceMethod: Int {
   static func isValidValue(interlaceMethod: Int) -> Bool {
     return interlaceMethod == 0 || interlaceMethod == 1
   }
+}
+
+public enum FilterType: Int {
+  case None = 0,
+    Sub = 1,
+    Up = 2,
+    Average = 3,
+    Paeth = 4
 }
